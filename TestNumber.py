aa7140550aa25b4c50eed3584af48553e9762c73
lirui315 <RuiLi_Violet@163.com>
2018-12-05 17:48:49 -0800

    change TestNumber

--- TestNumber.py
+++ TestNumber.py
@@ -22,9 +22,14 @@ class TestNumber(unittest.TestCase):
     def test_birthdate(self):
         self.assertMultiLineEqual(number.life_path_number(1996,3,15),"Your life path number is 7")
         self.assertMultiLineEqual(number.life_path_number(1990,10,25),"Your life path number is 9")
-        self.assertMultiLineEqual(number.life_path_number(1994,4,21),"Your life path number is 3")
-        self.assertMultiLineEqual(number.life_path_number(1995,11,6),"Your life path number is 5")
-        self.assertMultiLineEqual(number.life_path_number(1991,12,27),"Your life path number is 5")       
+        self.assertMultiLineEqual(number.life_path_number(1994,4,19),"Your life path number is 1")
+        self.assertMultiLineEqual(number.life_path_number(1975,11,29),"Your life path number is 8")
+        self.assertMultiLineEqual(number.life_path_number(1974,7,29),"Your life path number is 3") 
+        self.assertMultiLineEqual(number.life_path_number(0000,7,29),"Invalid Input") 
+        self.assertMultiLineEqual(number.life_path_number(1994,'a',29),"Invalid Input")
+        self.assertMultiLineEqual(number.life_path_number(1895,7,'c'),"Invalid Input")  
+        self.assertMultiLineEqual(number.life_path_number('d',7,29),"Invalid Input")   
+        self.assertMultiLineEqual(number.life_path_number(1974,7,2),"Your life path number is 3") 
 
     def tearDown(self):
         print('Tear Down')
