59f67805960c742ec8d7e9e4fec4f29855d0c948
lirui315 <RuiLi_Violet@163.com>
2018-12-05 19:12:04 -0800

    change testsuite

--- TestSuite.py
+++ TestSuite.py
@@ -4,27 +4,22 @@
 # In[2]:
 
 
-import testsuite.unittests.Agetest
 
 
 # In[4]:
 
 
 import unittest
-import testsuite.unittests.TestBirthdateMonth as m
-import testsuite.unittests.TestBirthdateYear as y
-import testsuite.unittests.Agetest as a
-import testsuite.unittests.Nametest as n
+import TestBirthdate as b
+import TestNumber as N
 
 
 
 def my_suite():
     suite = unittest.TestSuite()
     result = unittest.TestResult()
-    suite.addTest(unittest.makeSuite(m.TestBirthdateMonth))
-    suite.addTest(unittest.makeSuite(y.TestBirthdateYear))
-    suite.addTest(unittest.makeSuite(a.TestAge))
-    suite.addTest(unittest.makeSuite(n.TestName))
+    suite.addTest(unittest.makeSuite(b.TestBirthdate))
+    suite.addTest(unittest.makeSuite(N.TestNumber))
     runner = unittest.TextTestRunner()
     print(runner.run(suite))
 my_suite()
